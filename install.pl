ed932c5087ef711142a33430823bee43f494ff67
landreev <leonid@hmdc.harvard.edu>
2015-07-19 23:46:18 -0400

    Update install.pl
    Added 2 lines to the installer script, to print out the URL of the TwoRavens app - so that the user could configure it in the settings of their Dataverse app.

--- install.pl
+++ install.pl
@@ -267,3 +267,6 @@ system ("/bin/mv " . $TwoRavensWebApp . ".tmp " . $TwoRavensWebApp);
 system ("chown -R apache " . $CONFIG_DEFAULTS{'TWORAVENS_DIRECTORY'});
 
 print "\n\nGreat. You should now have a working TwoRavens installation!\n";
+print "\nThe application URL is \n";
+print $RAPACHEURL . "/dataexplore\n\n";
+
