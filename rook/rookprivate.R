52d55ff4a79fc07cefee227943fe488e9160e289
tercer <jameshonaker2@gmail.com>
2016-12-05 15:29:57 -0500

    changes to prototype budgeter v2

--- rook/rookprivate.R
+++ rook/rookprivate.R
@@ -1,52 +1,106 @@
 ##
-##  rookPrivate
+##  rookprivate
 ##
 ##  Rook apps for calling differential privacy modules and updating accuracy table on ingest of private data
+##  Initialized by rookSetup in development mode.  In production, called by TwoRaven's rApache.
 ##
-##  8/19/15 jH
+##  12/11/14 jH
 ##
-
 
 
 
 privateStatistics.app <-function(env){
-
     production<-FALSE     ## Toggle:  TRUE - Production, FALSE - Local Development
 
     if(production){
         sink(file = stderr(), type = "output")
     }
+
+    print("Entered privateStatistics app")
+    
     request <- Request$new(env)
     response <- Response$new(headers = list( "Access-Control-Allow-Origin"="*"))
 
-    ## The names of the objects will depend on your request
-    everything <- jsonlite::fromJSON(request$POST()$tableJSON)
-    print(everything)
+    ## Run some checking on the inputs to make certain values are valid and release can go ahead
+    warning <- FALSE
+    message <- "nothing"
+    
+    valid <- jsonlite::validate(request$POST()$tableJSON)
+    print(valid)
 
-    ## Run some checking on the inputs to make valid and can go ahead
-    warning<-FALSE
-    message<-"nothing"
+    ## Check the POST sent to the app appears to be valid JSON
+    if(!valid) {
+        warning <- TRUE
+        result <- list(warning="The request is not valid json. Check for special characters.")
+    }
+    
+    ## Translate the JSON into R types
+    if(!warning) {
+        everything <- jsonlite::fromJSON(request$POST()$tableJSON)
+        print(everything)
 
-    if(!warning){
-        df <- everything$df				# table as a data.frame
+        # Unpack JSON into component parts
+        dict <- everything$dict
+        indices <- everything$indices
+        stats <- everything$stats
+        metadata <- everything$metadata
+        globals <- everything$globals
         fileid <- everything$fileid
-        globals <- everything$globals	# dictionary of global parameters eps, del, beta
-      									# and eventually n. 
-        #data <- GET_THIS_FROM_DATAVERSE(fileid)
-        print(data)
-        metadata <- calculate_stats(data, df, globals)
+    }
 
-        result <- jsonlite:::toJSON(metadata, digits=8)
+    ## Check the epsilons budgeted for the statistics come under the global epsilon
+    compositioncheckready <- FALSE
+	if(!warning & compositioncheckready){
+		warning <- TRUE
+		message <- "The individual epsilons budgeted across the list of statistics do not collectively compose below the global epsilon."
+	}
+
+    ## Get dataset from Dataverse
+    ## Compare to data.app in TwoRavens and decide if this could be done more robustly
+    if(!warning){
+
+        # file id supplied; we are going to assume that we are dealing with
+        # a dataverse and cook a standard dataverse data access url,
+        # with the fileid supplied and the hostname we have
+        # either supplied or configured:
+
+        dataurl <- paste("https://beta.dataverse.org/api/access/datafile/", fileid, sep="")
+        # Might need to investigate adding an apikey:
+        #dataurl = dataurl+"?key="+apikey;
+ 
+        # This to write locally
+        # data <- download.file(dataurl, destfile = "\tmp\test.tab", method="curl", extra=c("--insecure"))
+
+        ## This is simplest, but fragile
+        data <- read.table(dataurl, header=TRUE, fill=TRUE) 
+        cat("data successfully downloaded from Dataverse \n")
+        print(data[1:5,])
+        cat("---------------- \n")
+
+    }    
+
+    ## Generate differentially private values and return released statistics as JSON
+    if(!warning){
+        df <- convert(dict, indices, stats, metadata)
+        cat("Cleared table conversion \n")
+        
+        releasedMetadata <- calculate_stats(data, df, globals)
+        cat("Cleared release function \n")
+
+        result <- jsonlite:::toJSON(releasedMetadata, digits=8)
+        cat("Cleared JSON conversion \n")
+
     }else{
-        result<- jsonlite:::toJSON(message)
+        result <- jsonlite:::toJSON(message)
     }
 
     print(result)
     cat("\n")
-    
+   
     if(production){
         sink()
     }
+
     response$write(result)
     response$finish()
 }
@@ -55,171 +109,215 @@ privateStatistics.app <-function(env){
 
 
 privateAccuracies.app <- function(env){
-
     production<-FALSE     ## Toggle:  TRUE - Production, FALSE - Local Development
     
     if(production){
         sink(file = stderr(), type = "output")
     }
+
+    print("Entered Accuracies app")
    
     request <- Request$new(env)
     response <- Response$new(headers = list( "Access-Control-Allow-Origin"="*"))
-  
-    ## The names of the objects will depend on your request
-    everything <- jsonlite::fromJSON(request$POST()$tableJSON)
-   
-    print(everything)
+
+
+    ## Run some checking on the inputs to make certain values are valid and composition can go ahead
+    warning <- FALSE
+    message <- "nothing"
     
-    ## Run some checking on the inputs to make valid and can go ahead
-    warning<-FALSE
-    message<-"Something went wrong"
-    
-    #first check contents of everything. then unpack them
-    if(is.null(everything)){
-    	warning <- TRUE
-    	message <- "Bad inputs"
+    valid <- jsonlite::validate(request$POST()$tableJSON)
+    print(valid)
+
+    if(!valid) {
+        warning <- TRUE
+        result <- list(warning="The request is not valid json. Check for special characters.")
     }
-      
-    df <- everything$df				# table as a data.frame
-    x <- everything$x 				# number -- or row name
-    y <- everything$y 				# number -- or col name
-    btn <- everything$btn			# identifier of which button was last pressed
-    globals <- everything$globals	# dictionary of global parameters eps, del, beta, n 
     
-   if(is.null(df) || is.null(x) || is.null(y) || is.null(btn) || is.null(globals)){
-   		warning <- TRUE 
-   		message <- "Bad inputs"
-   }
-    else{  				
-   		eps <- as.numeric(globals$eps)
-    	del <- as.numeric(globals$del)
-    	beta <- as.numeric(globals$beta)
-    	n <- as.numeric(globals$n)										
-   }
+    if(!warning) {
+        everything <- jsonlite::fromJSON(request$POST()$tableJSON)
+        print(everything)
+    }
+
+    
+    # #first check contents of everything. then unpack them
+    # if(is.null(everything)){
+    	# warning <- TRUE
+    	# message <- "Bad inputs"
+    # }
+      
+    # df <- everything$df				# table as a data.frame
+    # x <- everything$x 				# number -- or row name
+    # y <- everything$y 				# number -- or col name
+    # btn <- everything$btn			# identifier of which button was last pressed
+    # globals <- everything$globals	# dictionary of global parameters eps, del, beta, n 
+    
+   # if(is.null(df) || is.null(x) || is.null(y) || is.null(btn) || is.null(globals)){
+   		# warning <- TRUE 
+   		# message <- "Bad inputs"
+   # }
+    # else{  				
+   		# eps <- as.numeric(globals$eps)
+    	# del <- as.numeric(globals$del)
+    	# beta <- as.numeric(globals$beta)
+    	# n <- as.numeric(globals$n)										
+   # }
     
    	
-	if(!warning){    
-	    if(is.na(n) || n<=0 || n%%1 !=0){
-	 		warning <- TRUE
-	 		message <- "Invalid number of people in dataset"
-	 	}
+	# if(!warning){    
+	    # if(is.na(n) || n<=0 || n%%1 !=0){
+	 		# warning <- TRUE
+	 		# message <- "Invalid number of people in dataset"
+	 	# }
 	 	
-	    if(is.na(eps)){
-	    	warning <- TRUE
-	    	message <- "Epsilon must be a number"
-	    } 
-	    else if(eps <=0 || eps > 5){
-	    	warning <- TRUE
-	    	message <- "Epsilon out of range"
-	    }
+	    # if(is.na(eps)){
+	    	# warning <- TRUE
+	    	# message <- "Epsilon must be a number"
+	    # } 
+	    # else if(eps <=0 || eps > 5){
+	    	# warning <- TRUE
+	    	# message <- "Epsilon out of range"
+	    # }
 	    
-	    if(is.na(del)){
-	    	warning <- TRUE
-	    	message <- "Delta must be a number"
-	    }
+	    # if(is.na(del)){
+	    	# warning <- TRUE
+	    	# message <- "Delta must be a number"
+	    # }
 	    
-	    else if(del <=0 || del > .1){   #should we allow del=0? Choose upper bound.
-	    	warning <- TRUE
-	    	message <- "Delta out of range"
-	    }
+	    # else if(del <=0 || del > .1){   #should we allow del=0? Choose upper bound.
+	    	# warning <- TRUE
+	    	# message <- "Delta out of range"
+	    # }
 	    
-	    if(is.na(beta)){
-	    	warning <- TRUE
-	    	message <- "Beta must be a number"
-	    }
+	    # if(is.na(beta)){
+	    	# warning <- TRUE
+	    	# message <- "Beta must be a number"
+	    # }
 	 
-	 	else if(beta <=0 || beta > .5){
-	 		warning <- TRUE
-	    	message <- "Beta out of range"
-	 	}
-	} 	
+	 	# else if(beta <=0 || beta > .5){
+	 		# warning <- TRUE
+	    	# message <- "Beta out of range"
+	 	# }
+	# } 	
  	
  	
-    checkRow <- function(row){
-    	st <- row$Statistic
-		rowName <- row.names(row)
-		acc <- row$Accuracy
+    # checkRow <- function(row){
+    	# st <- row$Statistic
+		# rowName <- row.names(row)
+		# acc <- row$Accuracy
 		
-		#Accuracy can be blank if epsilon has not been assigned yet
-		if(!is.numeric(acc) && is.numeric(row$Epsilon)){
-			warning <- TRUE
-			message <- paste("In row",rowName,"Accuracy must be numeric")
-		}
+		# #Accuracy can be blank if epsilon has not been assigned yet
+		# if(!is.numeric(acc) && is.numeric(row$Epsilon)){
+			# warning <- TRUE
+			# message <- paste("In row",rowName,"Accuracy must be numeric")
+		# }
 		
-    	else if(is.null(st)){
-    		warning <- TRUE
-    		message <- paste("In row",rowName,"no statistic selected")
-    	}
+    	# else if(is.null(st)){
+    		# warning <- TRUE
+    		# message <- paste("In row",rowName,"no statistic selected")
+    	# }
     	
-        else if(st == "Histogram"){
-    		bins <- as.numeric(row$Numberofbins)
-    		if(is.na(bins) || bins%%1 !=0 || bins <=0){
-    			warning <- TRUE
-    			message <- paste("In row", rowName,"invalid number of bins.")
-    		}	
-    	}
+        # else if(st == "Histogram"){
+    		# bins <- as.numeric(row$Numberofbins)
+    		# if(is.na(bins) || bins%%1 !=0 || bins <=0){
+    			# warning <- TRUE
+    			# message <- paste("In row", rowName,"invalid number of bins.")
+    		# }	
+    	# }
     	
-    	else if(st == "Mean"){
-    		up <- as.numeric(row$UpperBound)
-    		lo <- as.numeric(row$LowerBound)
+    	# else if(st == "Mean"){
+    		# up <- as.numeric(row$UpperBound)
+    		# lo <- as.numeric(row$LowerBound)
     		
-    		if(is.na(up) || is.na(lo)){
-    			warning <- TRUE
-    			message <- paste("In row",rowName,"upper and lower bounds must be numbers")
-    		} 
-    		else if(up < lo){
-    			warning <- TRUE
-    			message <- paste("In row",rowName,"upper bound must be greater than lower bound")
-    		}
-    	}
+    		# if(is.na(up) || is.na(lo)){
+    			# warning <- TRUE
+    			# message <- paste("In row",rowName,"upper and lower bounds must be numbers")
+    		# } 
+    		# else if(up < lo){
+    			# warning <- TRUE
+    			# message <- paste("In row",rowName,"upper bound must be greater than lower bound")
+    		# }
+    	# }
     	
-    	else if (st == "Quantile"){
-    		up <- as.numeric(row$UpperBound)
-    		lo <- as.numeric(row$LowerBound)
-    		gran <- as.numeric(row$Granularity)
-    		if(is.na(up) || is.na(lo) || is.na(gran)){
-    			warning <- TRUE
-    			message <- paste("In row",rowName,"upper and lower bounds and granularity must be numbers")
-    		} 
-    		else if(up < lo){
-    			warning <- TRUE
-    			message <- paste("In row",rowName,"upper bound must be greater than lower bound")
-    		}
-    		else if(gran >= (up - lo) || gran <=0){
-    			warning <- TRUE
-    			message <- paste("In row",rowName,"invalid granularity")
-    		}
-    		
-    	}
-    	if(!warning){
-    		return("good")
-    	}
-    	else{
-    		return(message)
-    	}
-      }
+    	# else if (st == "Quantile"){
+    		# up <- as.numeric(row$UpperBound)
+    		# lo <- as.numeric(row$LowerBound)
+    		# gran <- as.numeric(row$Granularity)
+    		# if(is.na(up) || is.na(lo) || is.na(gran)){
+    			# warning <- TRUE
+    			# message <- paste("In row",rowName,"upper and lower bounds and granularity must be numbers")
+    		# } 
+    		# else if(up < lo){
+    			# warning <- TRUE
+    			# message <- paste("In row",rowName,"upper bound must be greater than lower bound")
+    		# }
+    		# else if(gran >= (up - lo) || gran <=0){
+    			# warning <- TRUE
+    			# message <- paste("In row",rowName,"invalid granularity")
+    		# }
+    	# }
+
+        # else if (st == "CDF"){
+            # up <- as.numeric(row$UpperBound)
+            # lo <- as.numeric(row$LowerBound)
+            # gran <- as.numeric(row$Granularity)
+            # if(is.na(up) || is.na(lo) || is.na(gran)){
+                # warning <- TRUE
+                # message <- paste("In row",rowName,"upper and lower bounds and granularity must be numbers")
+            # } 
+            # else if(up < lo){
+                # warning <- TRUE
+                # message <- paste("In row",rowName,"upper bound must be greater than lower bound")
+            # }
+            # else if(gran >= (up - lo) || gran <=0){
+                # warning <- TRUE
+                # message <- paste("In row",rowName,"invalid granularity")
+            # }
+        # }
+
+    	# if(!warning){
+    		# return("good")
+    	# }
+    	# else{
+    		# return(message)
+    	# }
+      # }
     
     
- if(!warning){   
-   # for(row in 1:nrow(df)){  # unblock to check every row. Can just check edited row
+ # if(!warning){   
+   # # for(row in 1:nrow(df)){  # unblock to check every row. Can just check edited row
   
-    	if(x != 0 && !is.na(df$Variable[x])){
-    		output <- checkRow(df[x,])
-    		if(output != "good"){
-    			warning <- TRUE
-    			message <- output
-    		}
-    	}
- 	 #} 
-  }
+    	# if(x != 0 && !is.na(df$Variable[x])){
+    		# output <- checkRow(df[x,])
+    		# if(output != "good"){
+    			# warning <- TRUE
+    			# message <- output
+    		# }
+    	# }
+ 	 # #} 
+  # }
     
-    
+   
     
 
     ## Here is the actual function of the app
     
     if(!warning){
-        prd <- GUI(df,x,y,btn,globals)
+    	### JM for small example
+    	dict <- everything$dict
+    	indices <- everything$indices
+    	# uncomment the next lines when the front end passes all the relevant info:
+    	stats <- everything$stats
+    	metadata <- everything$metadata
+    	globals <- everything$globals
+    	 action <- everything$action
+    	 var <- everything$var
+    	 stat <- everything$stat
+    	 prd <- callGUI(dict, indices, stats, metadata, globals, action, var, stat)
+    
+    	#prd <- callGUI(dict, indices)
+    	### end JM for small example 
+       # prd <- GUI(df,x,y,btn,globals) #JM 8/3
+      
         #test if prd returned an error
         if(class(prd) == "character"){
         	toSend <- list("error"="T", "message"=prd)
@@ -246,3 +344,7 @@ privateAccuracies.app <- function(env){
 
 
 
+# Other useful commands (see also "myrookrestart.R"):
+#R.server$browse("myrookapp")
+#R.server$stop()
+#R.server$remove(all=TRUE)
