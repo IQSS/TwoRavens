437947773a2c037e95a11eb21016ab7f76900e84
tercer <jameshonaker2@gmail.com>
2017-01-18 13:18:58 -0500

    set of revisions by Jack

--- rook/privacyfunctions/update_parameters.R
+++ rook/privacyfunctions/update_parameters.R
@@ -4,11 +4,11 @@
 # privacy parameters (epsilon, delta) pair is added, removed, or changed, 
 # all of the other parameters must also change in such a way as to preserve
 # the global privacy budget. Currently this works as follows: assign any new
-# parameters values as though we were splitting the budget completely evenly. 
-# Fix these values along with any values that the user has marked as fixed (Hold feature). 
-# Scale all of the other parameters by the same multiplicative factor, r. Do
-# binary search on values of r, checking the output of the optimal composition
-# theorem on each iteration until the global epsilon is reached.  
+# parameters values as though we were splitting the budget completely evenly among 
+# unheld statistics. Fix these values and the held values. Scale all of the other 
+# parameters by the same multiplicative factor, r. Do binary search on values of r,
+# checking the output of the optimal composition theorem on each iteration until the 
+# global epsilon is reached.  
 # 
 # 
 # Jack Murtagh
@@ -37,7 +37,7 @@ update_parameters <- function(params, hold, eps, del){
 	#	kx2 matrix of updated parameters
 	
 	
-	# k is the total number of calls the user wishes to make to a DP algorithm
+	# k is the total number of statistics currently selected
 	k <- length(params[ , 1])
 		
 	# If we are only calculating one statistic, allocate the whole budget to it.
@@ -49,6 +49,7 @@ update_parameters <- function(params, hold, eps, del){
 
 	elist <- as.numeric(params[ ,1])
 	dlist <- as.numeric(params[ ,2])
+	#hard coded error tolerance for optimal composition approximation. Might do something more clever one day. 
 	err <- eps/10
 	
 	# Check if there are unset epsilon values
@@ -94,12 +95,11 @@ update_parameters <- function(params, hold, eps, del){
 			return("error")
 		}
 	}
+	
 	newElist <- scale_eps(elist, dlist, eps, del, free, err)
 	toReturn <- cbind(newElist, dlist)
 	return(toReturn)
 }
-	
-	
 	
 
 scale_eps <- function(elist, dlist, eps, del, free, err){
@@ -110,10 +110,13 @@ scale_eps <- function(elist, dlist, eps, del, free, err){
 	
 	# Initialize parameters for binary search. 
 	l <- 0
-	u <- eps/min(elist)
+	u <- eps/max(elist)  # no epsilon value in the composition can exceed global eps
 	dp <- F
-	# come up with better stopping condition
-	while(u-l>.01 || !dp){	
+	
+	goodlist <- c()
+	
+	# is there a better stopping condition?
+	while(u-l>.0001){	
 		# scaling factor		
 		r <- l + ((u - l)/2)
 		testElist <- elist
@@ -124,12 +127,21 @@ scale_eps <- function(elist, dlist, eps, del, free, err){
 		# Reset upper and lower bounds of the search depending on outcome of isDP
 		if(dp){
 			l <- r
+			goodlist <- testElist
 		}
 		else {
 			u <- r
 		}
 	}
-	return(testElist)	
+	# If result does not beat simple summing composition
+	total <- sum(goodlist)
+	if(total < eps){
+		toadd <- (eps-total)/length(free)
+		toaddlist <- rep(0,times=length(goodlist))
+		toaddlist[free] <- toadd
+		goodlist <- goodlist + toaddlist
+	}
+	return(goodlist)	
 }
 
 
