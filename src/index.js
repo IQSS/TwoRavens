b368826e97a9637f065f2f0d9ebed0184d7015a5
aaron <aaron@tex.my.domain>
2017-05-23 01:08:05 -0500

    moved <script> to oncreate

--- src/index.js
+++ src/index.js
@@ -192,13 +192,84 @@ function rightpanel() {
     ]);
 }
 
+let fileid = "";
+let hostname = "";
+let apikey = "";
+let ddiurl = "";
+let dataurl = "";
+
 class Body {
-	oncreate() {
-		require('./script.js');	
-		require('../app_ddi.js');	
-	}
-	
-	view() {
+    oncreate() {
+        $('#leftpanel span').click(function() {
+            if (!$('#leftpanel').hasClass('forceclosepanel')) {
+                $('#leftpanel').removeClass('expandpanel');
+                $('#leftpanel > div.row-fluid').toggleClass('closepanel');
+                $('#leftpanel').toggleClass('closepanel');
+                $('#main').toggleClass('svg-leftpanel');
+                $('#btnSelect').css('display', 'none');
+            }
+        });
+        $('#rightpanel span').click(function() {
+            if (!$('#leftpanel').hasClass('forceclosepanel')) {
+                $('#rightpanel').removeClass('expandpanel');
+                $('#rightpanel > div.row-fluid').toggleClass('closepanel');
+                $('#rightpanel').toggleClass('closepanel');
+                $('#main').toggleClass('svg-rightpanel');
+            }
+        });
+
+        let myurl = window.location.toString();
+        let cindex = 0;
+        if (myurl.indexOf("dfId=") > 0) {
+            fileid = myurl.substring(myurl.indexOf("dfId=") + 5);
+            cindex = fileid.indexOf("&");
+            if (cindex > 0) {
+                fileid = fileid.substring(0, cindex);
+            };
+        };
+        if (myurl.indexOf("host=") > 0) {
+            hostname = myurl.substring(myurl.indexOf("host=") + 5);
+            cindex = hostname.indexOf("&");
+            if (cindex > 0) {
+                hostname = hostname.substring(0, cindex);
+            };
+        };
+        if (myurl.indexOf("key=") > 0) {
+            apikey = myurl.substring(myurl.indexOf("key=") + 4);
+            cindex = apikey.indexOf("&");
+            if (cindex > 0) {
+                apikey = apikey.substring(0, cindex);
+            };
+        };		
+        if (myurl.indexOf("ddiurl=") > 0) {
+            ddiurl = myurl.substring(myurl.indexOf("ddiurl=") + 7);
+            ddiurl = ddiurl.replace(/%25/g, "%");
+            ddiurl = ddiurl.replace(/%3A/g, ":");
+            ddiurl = ddiurl.replace(/%2F/g, "/");
+            cindex = ddiurl.indexOf("&");
+            if (cindex > 0) {
+                ddiurl = ddiurl.substring(0, cindex);
+            };
+        };		
+        if (myurl.indexOf("dataurl=") > 0) {
+            dataurl = myurl.substring(myurl.indexOf("dataurl=") + 8);
+            dataurl = dataurl.replace(/%25/g, "%");
+            dataurl = dataurl.replace(/%3A/g, ":");
+            dataurl = dataurl.replace(/%2F/g, "/");
+            cindex = dataurl.indexOf("&");
+            if (cindex > 0) {
+                dataurl = dataurl.substring(0, cindex);
+            };
+        };
+
+        console.log("fileid: " + fileid);
+        console.log("hostname: " + hostname);
+        console.log("apikey: " + apikey);
+        console.log("ddiurl: " + ddiurl);
+        console.log("dataurl: " + dataurl);
+    }
+
+    view() {
         return m('main',
             m("nav.navbar.navbar-default[id='option'][role='navigation']",
                 m("div", [
