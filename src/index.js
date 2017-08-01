3b22725ff22ecd5ac7af818b3001cf6afc4ebc9a
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2017-07-31 23:15:03 -0500

    cleaned up panels

--- src/index.js
+++ src/index.js
@@ -16,71 +16,68 @@ let Model = {
     toggleLegend: false
 };
 
-function subpanel(title, buttons=[]) {
-    let legend = title == "Legend";
+let subpanel = function(title, buttons=[]) {
+    let legend = title === 'Legend';
     let [target, toggle] = ['collapse' + title, 'toggle' + title];
     let z = app.zparams;
     return m(`#${legend ? "legend.legendary" : "logdiv.logbox"}.panel.panel-default`, {
-        style: {display: legend && z.ztime.length + z.zcross.length + z.zdv.length + z.znom.length || !legend && app.logArray.length > 0 ? 'block' : 'none'}
-    }, [m(".panel-heading",
-          m("h3.panel-title",
-            title,
-            m(`span.glyphicon.glyphicon-large.glyphicon-chevron-${Model[toggle] ? 'up' : 'down'}.pull-right[data-target=#${target}][data-toggle=collapse][href=#${target}]`, {
-                style: 'cursor: pointer',
-                onclick: _ => Model[toggle] = !Model[toggle]}))),
-        m(`#${target}.panel-collapse.collapse.in`,
-          m(".panel-body", !legend ? app.logArray.map(x => m('p', x)) : buttons.map(x => {
-              return m(`#${x[0]}.clearfix.${z[x[1]].length == 0 ? "hide" : "show"}`, [
-                  m(".rectColor",
-                    m("svg[style=width: 20px; height: 20px]",
-                      m("circle[cx=10][cy=10][fill=white][r=9][stroke=black][stroke-width=2]"))),
-                  m(".rectLabel", x[2])]);})))]);
-}
-
-let closepanel = (val, side) => {
-    if (Model[side + 'Closed']) return val + '.closepanel';
-    return side == 'left' && app.lefttab == 'tab2' ? val + '.expandpanel' : val;
+        style: {display: legend && z.ztime.length + z.zcross.length + z.zdv.length + z.znom.length || !legend && app.logArray.length > 0 ? 'block' : 'none'}},
+             m(".panel-heading",
+               m("h3.panel-title",
+                 title,
+                 m(`span.glyphicon.glyphicon-large.glyphicon-chevron-${Model[toggle] ? 'up' : 'down'}.pull-right[data-target=#${target}][data-toggle=collapse][href=#${target}]`, {
+                     style: 'cursor: pointer',
+                     onclick: _ => Model[toggle] = !Model[toggle]}))),
+             m(`#${target}.panel-collapse.collapse.in`,
+               m(".panel-body", !legend ? app.logArray.map(x => m('p', x)) : buttons.map(x => {
+                   return m(`#${x[0]}.clearfix.${z[x[1]].length === 0 ? "hide" : "show"}`,
+                            m(".rectColor",
+                              m("svg[style=width: 20px; height: 20px]",
+                                m("circle[cx=10][cy=10][fill=white][r=9][stroke=black][stroke-width=2]"))),
+                            m(".rectLabel", x[2]));}))));
 };
 
-function panel(side, title, ...args) {
-    let id = `#${side}panel`;
-    let dot = m.trust('&#9679;');
-    let style = {style: {height: 'calc(100% - 60px)'}};
-    return m(closepanel(`#${side}panel.sidepanel.container.clearfix`, style, side), [
-        m(`#toggle${side == 'left' ? 'L' : 'R'}panelicon.panelbar`, style,
+let getClass = function(side) {
+    if (Model[side + 'Closed']) return '.closepanel';
+    return (side === 'left' && app.lefttab === 'tab2') ? '.expandpanel' : '';
+};
+
+let panel = function(side, title, ...args) {
+    let sidepanel = side + 'panel';
+    let dot = [m.trust('&#9679;'), m('br')];
+    return m(`#${sidepanel}.sidepanel.container.clearfix${getClass(side)}`, [
+        m(`#toggle${side === 'left' ? 'L' : 'R'}panelicon.panelbar[style=height: calc(100% - 60px)]`,
           m('span', {
               onclick(_) {
                   let key = side + 'Closed';
                   Model[key] = !Model[key];
-              }
-          }, [dot, m('br'), dot, m('br'), dot, m('br'), dot])),
-        m(`#${side}paneltitle.panel-heading.text-center`,
+              }}, [].concat([dot, dot, dot, dot]))),
+        m(`#${sidepanel}title.panel-heading.text-center`,
           m("h3.panel-title", title))
     ].concat(args));
-}
+};
 
 let or = function(side, val, y='block', n='none') {
     return app[side + 'tab'] === val ? y : n;
 };
 
-function leftpanel() {
+let leftpanel = function() {
     return panel(
         'left', 'Data Selection',
-        m(".btn-toolbar[role=toolbar][style=margin-left: .5em; margin-top: .5em",
+        m(".btn-toolbar[role=toolbar][style=margin-left: .5em; margin-top: .5em]",
           m(".btn-group",
             m(`button#btnVariables.btn.${or('left', 'tab1', 'active', 'btn-default')}[type=button]`, {
                 title: 'Click variable name to add or remove the variable pebble from the modeling space.',
-                onclick: _ => app.tabLeft('tab1')
-            }, "Variables"),
-            m(`button#btnSubset.btn.${or('left', 'tab2', 'active', 'btn-default')}[type=button]`, {
-                onclick: _ => app.tabLeft('tab2')
-            }, "Subset")),
+                onclick: _ => app.tabLeft('tab1')},
+              "Variables"),
+            m(`button#btnSubset.btn.${or('left', 'tab2', 'active', 'btn-default')}[type=button]`, {onclick: _ => app.tabLeft('tab2')},
+              "Subset")),
           m("button#btnSelect.btn.btn-default.ladda-button[data-spinner-color=#000000][data-style=zoom-in][type=button]", {
               style: `display: ${app.subset ? 'block' : 'none'}; float: right; margin-right: 10px`,
               onclick: _ => app.subsetSelect('btnSelect'),
-              title: 'Subset data by the intersection of all selected values.'
-          }, m("span.ladda-label[style=pointer-events: none]", "Select"))),
-        m(closepanel('.row-fluid', 'left'),
+              title: 'Subset data by the intersection of all selected values.'},
+            m("span.ladda-label[style=pointer-events: none]", "Select"))),
+        m('.row-fluid' + getClass('left'),
           m('#leftpanelcontent',
             m('#leftContentArea[style=height: 453px; overflow: auto]',
               m(`#tab1[style=display: ${or('left', 'tab1')}; padding: 10px 8px; text-align: center]`,
@@ -88,28 +85,30 @@ function leftpanel() {
               m(`#tab2[style=display: ${or('left', 'tab2')}; margin-top: .5em]`),
               m('#tab3',
                 m(`p[style=padding: .5em 1em; display: ${or('left', 'tab3')}]`, {
-                    title: "Select a variable from within the visualization in the center panel to view its summary statistics."
-                }, m('center',
+                    title: "Select a variable from within the visualization in the center panel to view its summary statistics."},
+                  m('center',
                      m('b', app.summary.name),
                      m('br'),
                      m('i', app.summary.labl)),
-                  m('table', app.summary.data.map(x => m('tr', x.map(y => m('td', {
-                      onmouseover: function() {this.style['background-color'] = 'aliceblue';},
-                      onmouseout: function() {this.style['background-color'] = '#f9f9f9';}
-                  }, y)))))))))));
-}
+                  m('table', app.summary.data.map(
+                      tr => m('tr', tr.map(
+                          td => m('td', {
+                              onmouseover: function() {this.style['background-color'] = 'aliceblue';},
+                              onmouseout: function() {this.style['background-color'] = '#f9f9f9';}},
+                             td)))))))))));
+};
 
-function rightpanel() {
-    let button = (id, width, text) => m(`button#${id}.btn.${or('right', id, 'active', 'btn-default')}[type=button][style=width: ${width}]`, {
-        onclick: _ => app.tabRight(id)
-    }, text);
+let rightpanel = function() {
+    let button = (id, width, text) =>
+        m(`button#${id}.btn.${or('right', id, 'active', 'btn-default')}[type=button][style=width: ${width}]`, {onclick: _ => app.tabRight(id)},
+          text);
     return panel(
         'right', 'Model Selection',
         m(".btn-group.btn-group-justified[aria-label=...][role=group][style=margin-top: .5em]",
           button('btnModels', "33%", "Models"),
           button('btnSetx', "34%", "Set Covar."),
           button('btnResults', "33%", "Results")),
-        m(closepanel('.row-fluid', 'right'),
+        m('.row-fluid' + getClass('right'),
           m('#rightpanelcontent',
             m('#rightContentArea[style=height: 453px; overflow: auto]',
               m(`#results[style=display: ${or('right', 'btnResults')}; margin-top: .5em]`,
@@ -118,7 +117,7 @@ function rightpanel() {
                 m("p#resultsHolder[style=padding: .5em 1em]")),
               m(`#setx[style=display: ${or('right', 'btnSetx')}]`),
               m(`#models[style=display: ${or('right', 'btnModels')}; padding: 6px 12px; text-align: center]`)))));
-}
+};
 
 class Body {
     oncreate() {
@@ -131,7 +130,7 @@ class Body {
             console.log(name, ': ', val);
             return val;
         };
-        let apikey = extract('apikey', 'key', 4);
+        // let apikey = extract('apikey', 'key', 4);
         app.main(
             extract('fileid', 'dfId', 5),
             extract('hostname', 'host', 5),
@@ -157,17 +156,15 @@ class Body {
                        m('h4#dataName[style=display: inline]', {
                            onclick: _ => Model.cite = Model.citetoggle = !Model.citetoggle,
                            onmouseout: _ => Model.citetoggle || (Model.cite = false),
-                           onmouseover: _ => Model.cite = true
-                       }, "Dataset Name"),
+                           onmouseover: _ => Model.cite = true},
+                         "Dataset Name"),
                        m(`#cite.panel.panel-default[style=display: ${Model.cite ? 'block' : 'none'}; position: absolute; right: 50%; width: 380px; text-align: left; z-index: 50]`,
                          m(".panel-body")),
-                       m("button#btnEstimate.btn.btn-default.ladda-button.navbar-right[data-spinner-color=#000000][data-style=zoom-in]", {
-                           onclick: _ => app.estimate('btnEstimate'),
-                           style: "margin-left: 2em; margin-right: 1em"
-                       }, m("span.ladda-label", "Estimate")),
-                       m("button#btnReset.btn.btn-default.navbar-right[title=Reset][style=margin-left: 2.0em]", {
-                           onclick: app.reset
-                       }, m("span.glyphicon.glyphicon-repeat[style=color: #818181; font-size: 1em; pointer-events: none]")),
+                       m("button#btnEstimate.btn.btn-default.ladda-button.navbar-right[data-spinner-color=#000000][data-style=zoom-in][style=margin-left: 2em; margin-right: 1em]", {
+                           onclick: _ => app.estimate('btnEstimate')},
+                         m("span.ladda-label", "Estimate")),
+                       m("button#btnReset.btn.btn-default.navbar-right[title=Reset][style=margin-left: 2.0em]", {onclick: app.reset},
+                         m("span.glyphicon.glyphicon-repeat[style=color: #818181; font-size: 1em; pointer-events: none]")),
                        m('#transformations.transformTool', {
                            title: 'Construct transformations of existing variables using valid R syntax. For example, assuming a variable named d, you can enter "log(d)" or "d^2".'}))),
                    /*m('.text-center', {
@@ -181,14 +178,13 @@ class Body {
                        'TwoRavens v0.1 "Dallas" -- The Norse god Odin had two talking ravens as advisors, who would fly out into the world and report back all they observed. In the Norse, their names were "Thought" and "Memory". In our coming release, our thought-raven automatically advises on statistical model selection, while our memory-raven accumulates previous statistical models from Dataverse, to provide cummulative guidance and meta-analysis.'))),
                  m(`#main.left.carousel.slide${Model.leftClosed ? '.svg-leftpanel' : ''}${Model.rightClosed ? '.svg-rightpanel' : ''}[style=overflow: auto]`,
                    m("#innercarousel.carousel-inner",
-                     m('#m0.item.active', m('svg#whitespace'))),
-                   m("#spacetools.spaceTool[style=z-index: 16]", 
-                     m("button#btnForce.btn.btn-default[title=Pin the variable pebbles to the page.]", {
-                         onclick: app.forceSwitch
-                     }, m("span.glyphicon.glyphicon-pushpin")),
-                     m("button#btnEraser.btn.btn-default[title=Wipe all variables from the modeling space.]", {
-                         onclick: app.erase
-                     }, m("span.glyphicon.glyphicon-magnet"))),
+                     m('#m0.item.active',
+                       m('svg#whitespace'))),
+                   m("#spacetools.spaceTool[style=z-index: 16]",
+                     m("button#btnForce.btn.btn-default[title=Pin the variable pebbles to the page.]", {onclick: app.forceSwitch},
+                       m("span.glyphicon.glyphicon-pushpin")),
+                     m("button#btnEraser.btn.btn-default[title=Wipe all variables from the modeling space.]", {onclick: app.erase},
+                       m("span.glyphicon.glyphicon-magnet"))),
                    subpanel("Legend", [
                        ['timeButton', 'ztime', 'Time'],
                        ['csButton', 'zcross', 'Cross Sec'],
