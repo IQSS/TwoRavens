ef5649ee5cc4712af2681b7901965f4b76923c7c
Raman Prasad <raman_prasad@harvard.edu>
2017-08-21 18:07:43 -0400

    stop caching of preprocess file. #14

--- rook/rooktransform.R
+++ rook/rooktransform.R
@@ -17,19 +17,19 @@ transform.app <- function(env){
 
     request <- Request$new(env)
     response <- Response$new(headers = list( "Access-Control-Allow-Origin"="*"))
-    
+
     valid <- jsonlite::validate(request$POST()$solaJSON)
     print(valid)
     if(!valid) {
         warning <- TRUE
         result <- list(warning="The request is not valid json. Check for special characters.")
     }
-    
+
     if(!warning) {
         everything <- jsonlite::fromJSON(request$POST()$solaJSON)
         print(everything)
     }
-    
+
     if(!warning){
         mysessionid <- everything$zsessionid
         mylogfile<-logFile(mysessionid, production)
@@ -38,7 +38,7 @@ transform.app <- function(env){
             result <- list(warning="No session id.")
         }
     }
-    
+
     if(!warning){
         if(production){
             mydata <- readData(sessionid=mysessionid,logfile=mylogfile)
@@ -49,7 +49,7 @@ transform.app <- function(env){
             #mydata <- read.delim("../data/QualOfGovt.tsv")
         }
 	}
-    
+
     if(!warning){
 		myvars <- everything$zvars
         if(is.null(myvars)){
@@ -57,7 +57,7 @@ transform.app <- function(env){
             result<-list(warning="Problem with variables.")
         }
 	}
-    
+
     if(!warning){
         history <- everything$callHistory
         if(is.null(history)){
@@ -65,7 +65,7 @@ transform.app <- function(env){
             result<-list(warning="callHistory is null.")
         }
     }
-    
+
     if(!warning){
         typeTransform <- everything$typeTransform
         if(!is.logical(typeTransform)){
@@ -73,7 +73,7 @@ transform.app <- function(env){
             result<-list(warning="typeTransform must be a logical.")
         }
     }
-    
+
     if(!warning){
 		myT <- everything$transform
         if(is.null(myT) & !typeTransform){
@@ -81,7 +81,7 @@ transform.app <- function(env){
             result<-list(warning="Invalid transformation.")
         }
 	}
-    
+
     if(!warning){
         typeStuff <- everything$typeStuff
         if(!is.list(typeStuff) | (is.null(typeStuff$varnames) | is.null(typeStuff$interval) | is.null(typeStuff$numchar) | is.null(typeStuff$nature) | is.null(typeStuff$binary))){
@@ -89,19 +89,19 @@ transform.app <- function(env){
             result<-list(warning="typeStuff is not a list or one of the necessary elements---varnames, interval, numchar, nature, binary---is null.")
         }
     }
-    
+
     # transforming a variable's type
     if(!warning & typeTransform) {
         tryCatch(
         {
             ## 1. prepare mydata so that it is identical to the representation of the data in TwoRavens
             mydata <- executeHistory(history=history, data=mydata)
-            
+
             ## 2. calculate summary statistics
             tdata <- as.data.frame(mydata[,myvars])
             colnames(tdata) <- myvars
             print(class(tdata))
-            
+
             # preprocess just one variable
             purl <- pCall(data=tdata, production, sessionid=mysessionid, types=typeStuff)
             result<- jsonlite:::toJSON(list(url=purl, typeTransform=typeTransform))
@@ -119,22 +119,23 @@ transform.app <- function(env){
             assign("result", result, envir=globalenv())
         })
     }
-    
+
     # creating a new variable with a transformation function
     if(!warning & !typeTransform) {
         tryCatch(
         {
             ## 1. prepare mydata so that it is identical to the representation of the data in TwoRavens
             mydata <- executeHistory(history=history, data=mydata)
-            
+
             ## 2. make the current transformation
             tdata <- parseTransform(data=mydata, func=myT, vars=myvars)
             call<-colnames(tdata)
-            
+
             # preprocess just one variable
             colnames(tdata) <- call
             purl <- pCall(data=tdata, production, sessionid=mysessionid, types=NULL)
-            result<- jsonlite:::toJSON(list(call=call, url=purl, trans=c(myvars,myT), typeTransform=typeTransform))
+
+            result<- jsonlite:::toJSON(list(call=call, url=paste(purl, getTimestampForUrl(), sep=""), trans=c(myvars,myT), typeTransform=typeTransform))
         },
         error=function(err){
             warning <<- TRUE
@@ -149,7 +150,7 @@ transform.app <- function(env){
             assign("result", result, envir=globalenv())
         })
     }
-    
+
     print(result)
     if(production){
         sink()
@@ -157,4 +158,3 @@ transform.app <- function(env){
     response$write(result)
     response$finish()
 }
-
